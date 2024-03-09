f1e4df878e66c6f5ddb28d147aa96bfd9c35bd7c
VasiliL <basil.lozhkin@gmail.com>
2024-03-09 05:58:55 +0500

    убрал @property из модели Run. Привёл названия атрибутов в соответствие полям таблицы SQL. Требуется рефакторинг фронта, чтобы он отправлял null вместо пустых строк для атрибутов с необязательными датами. Refactor front_interaction.py to simplify key extraction
    Simplified the select query generation in the front_interaction.py by extracting keys from __annotations__ directly, removing the method to drop underscores. Also, the properties with underscore prefixes are directly converted into attributes, eliminating the need of setters and getters for date attributes.

--- models/front_interaction.py
+++ models/front_interaction.py
@@ -8,16 +8,10 @@ from decimal import Decimal
 class MyModel(BaseModel):
     @classmethod
     def generate_select_query(cls):
-        column_names = [cls.drop_underline(key) for key in cls.__annotations__.keys()]
+        column_names = list(cls.__annotations__.keys())
         return sql.SQL("SELECT {}").format(
             sql.SQL(",").join(map(sql.Identifier, column_names))
         )
-
-    @classmethod
-    def drop_underline(cls, string: str) -> str:
-        while string.startswith('_'):
-            string = string[1:]
-        return string
 
 
 class Car(MyModel, BaseModel):
@@ -70,40 +64,11 @@ class Run(MyModel, BaseModel):
     invoice_document: Optional[str] = None
     waybill: Optional[str] = None
     weight: Optional[Decimal] = Decimal(0)
-    _date_arrival: Optional[date] = None
+    date_arrival: Optional[date] = None
     reg_number: Optional[str] = None
-    _reg_date: Optional[date] = None
+    reg_date: Optional[date] = None
     acc_number: Optional[str] = None
-    _acc_date: Optional[date] = None
+    acc_date: Optional[date] = None
     client: Optional[str] = None
     route: Optional[str] = None
     cargo: Optional[str] = None
-
-    @property
-    def reg_date(self) -> datetime.date:
-        return self._reg_date
-
-    @reg_date.setter
-    def reg_date(self, value: str) -> None:
-        self._reg_date = datetime.strptime(value, "%Y-%m-%d").date()
-
-    @property
-    def acc_date(self) -> datetime.date:
-        return self._reg_date
-
-    @acc_date.setter
-    def acc_date(self, value: str) -> None:
-        self._reg_date = self.str_to_date(value)
-
-    @property
-    def date_arrival(self) -> datetime.date:
-        return self._reg_date
-
-    @date_arrival.setter
-    def date_arrival(self, value: str) -> None:
-        self._reg_date = self.str_to_date(value)
-
-    @staticmethod
-    def str_to_date(date_str: str) -> datetime.date:
-        if date_str:
-            return datetime.strptime(date_str, "%Y-%m-%d").date()
