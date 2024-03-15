0c58cbb05351d80ae38482fcef93b70433312f2a
VasiliL <basil.lozhkin@gmail.com>
2024-03-15 17:44:09 +0500

    Update "cars" table references in front_interaction.py
    The reference list for the "cars" table in front_interaction.py was expanded with an additional reference, "_inforg13254". This change follows the necessity to include more detailed information about cars in the system processing.

--- routes/front_interaction.py
+++ routes/front_interaction.py
@@ -12,7 +12,7 @@ from components.func import post_multiple_objects, put_multiple_objects
 router = APIRouter()
 
 TABLES = {"persons": ("_reference300", "_reference155", "_reference89", "_inforg13301"),
-          "cars": ("_reference262", "_reference211", "_reference259"),
+          "cars": ("_reference262", "_reference211", "_reference259", "_inforg13254"),
           "invoices": ("_document350", "_document350_vt1855", "_document365_vt2454", "_reference124", "_reference207",
                        "_reference207_vt7419", "_reference225", "_reference111", "_reference110", "_reference128")}
 STATIC_VIEWS = {"persons", "cars", "cargo", "routes", "counterparty", "invoices", "react_drivers", "react_cars",
